dec66db5d5257e9363a9ebc7f44fd67dd78b3c52
margotgoudard <goudard.margot@hotmail.com>
2024-10-09 12:08:44 +0200

    Login

--- App.js
+++ App.js
@@ -1,49 +1,29 @@
+// App.js
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './src/redux/store';
-import Counter from './src/components/Counter';
-
-export default function App() {
-  return (
-    <Provider store={store}>
-      <Counter />
-    </Provider>
-  );
-}
-
-/*
-import { StyleSheet, Text, View } from 'react-native';
-import LoginPage from './screens/LoginPage.js';
-import RegisterPage from './screens/RegisterPage.js';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Login from './src/screens/Login';
 
-const Stack  = createStackNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
+function AppContent() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated); 
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen 
-          name="Login" 
-          component={LoginPage} 
-          options={{ headerShown: false }} 
-        />
-        <Stack.Screen name="Register" component={RegisterPage}  options={{ headerShown: false }}  />
-        <Stack.Screen name="Profil" component={ProfilePage}  options={{ headerShown: false }}  />
+        <Stack.Screen name="Login" component={Login}  options={{ headerShown: false }}  />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-*/
+export default function App() {
+  return (
+    <Provider store={store}>
+      <AppContent />
+    </Provider>
+  );
+}
---
+++ src/screens/Login.js
@@ -0,0 +1,175 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, Alert } from 'react-native';
+import axios from 'axios';
+import { API_BASE_URL } from '@env';
+import { useDispatch } from 'react-redux';  
+import { login } from '../redux/authSlice'; 
+
+const Login = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [serverError, setServerError] = useState(''); 
+
+  const dispatch = useDispatch();
+
+  const handleLogin = async () => {
+    let isValid = true;
+
+    setEmailError('');
+    setPasswordError('');
+    setServerError(''); 
+
+    if (!email) {
+      setEmailError('Adresse mail est obligatoire');
+      isValid = false;
+    }
+
+    if (!password) {
+      setPasswordError('Mot de passe est obligatoire');
+      isValid = false;
+    }
+
+    if (!isValid) return;
+
+    try {
+      const response = await axios.post(`${API_BASE_URL}/login`, {
+        email: email,
+        password: password,
+      });
+
+      if (response.status === 200) {
+        dispatch(login(response.data)); 
+        // navigation.navigate('Home');
+      }
+    } catch (error) {
+        const errorMessage = 'Vérifiez vos identifiants de connexion';
+        setServerError(errorMessage); 
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image source={require('../../assets/logo.png')} style={styles.logo} />
+      </View>
+
+      <Text style={styles.title}>Connexion</Text>
+
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Adresse mail"
+        style={[styles.input, emailError ? styles.errorInput : null]}
+        keyboardType="email-address"
+      />
+      {emailError ? <Text style={styles.errorText}>{emailError}</Text> : null}
+
+      <TextInput
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Mot de passe"
+        secureTextEntry={true}
+        style={[styles.input, passwordError ? styles.errorInput : null]}
+      />
+      {passwordError ? <Text style={styles.errorText}>{passwordError}</Text> : null}
+
+      {serverError ? <Text style={styles.serverErrorText}>{serverError}</Text> : null}
+
+      <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+        <Text style={styles.signupLink}>
+          Vous n'avez pas encore de compte ? <Text style={styles.signupText}>S’inscrire</Text>
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+        <Text style={styles.loginButtonText}>Se connecter</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 30,
+    marginTop: -100,
+    width: '70%',
+    height: '20%',
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#173054',
+    width: '100%',
+    textAlign: 'left',
+  },
+  input: {
+    backgroundColor: '#E4F4FF',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+    fontSize: 16,
+  },
+  errorInput: {
+    borderColor: 'red',
+    borderWidth: 1,
+  },
+  errorText: {
+    color: 'red',
+    marginBottom: 10,
+    textAlign: 'left',
+    width: '100%',
+  },
+  serverErrorText: {
+    color: 'red',
+    marginBottom: 10,
+    marginLeft: '2%',
+    width: '100%',
+  },
+  signupLink: {
+    fontSize: 14,
+    fontStyle: 'italic',
+    color: '#666',
+    marginBottom: 20,
+    width: '100%',
+    textAlign: 'left',
+  },
+  signupText: {
+    color: '#1E5AFF',
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    textDecorationLine: 'underline',
+  },
+  loginButton: {
+    backgroundColor: '#F78E3B',
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    borderRadius: 20,
+    width: '70%',
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default Login;
