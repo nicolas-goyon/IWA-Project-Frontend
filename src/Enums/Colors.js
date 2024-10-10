01fbfc8aeedc74b2bf1c599a34e4d4796370d7b4
nicolas.goyon <nicolas.goyon@etu.umontpellier.fr>
2024-10-10 08:26:04 +0200

    Refactor: Couleurs

---
+++ src/Enums/Colors.js
@@ -0,0 +1,11 @@
+const Colors = {
+    NavyBlue: '#173054',
+    White : '#fff',
+    LightBlue: '#E4F4FF',
+    Red: 'red',
+    Gray: '#666',
+    Blue: '#1E5AFF',
+    Orange : '#F78E3B'
+}
+
+export default Colors;
